1cc89214becc9c4439cdcca970d6283552d9c1ad
pix <thatpixguy@gmail.com>
2014-01-02 23:22:21 +1030

    made build.sh a valid shell script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 javac -classpath RXTXcomm.jar A4S.java processing/src/Firmata.java
-mkdir org/firmata
-copy processing/src/*.class org/firmata/
+mkdir -p org/firmata
+cp processing/src/*.class org/firmata/
 jar -cfm A4S.jar manifest.mf *.class org/firmata/*.class
-del *.class org/firmata/*.class
+rm *.class org/firmata/*.class
